30f225afbd75640308962174495c9bab45578059
Shelley Brook <sbrook13@gmail.com>
2020-07-20 23:11:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,10 +10,11 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       if !name_hash[key]
         return key_to_report
-      else  
-        if name_hash[key] < value_comparison 
+      else
+        if  name_hash[key] < value_comparison 
         value_comparison = name_hash[key]
         key_to_report = key 
+        end
       end  
     end
   key_to_report 
